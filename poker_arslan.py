85d5c4766f4b6ccc55e02bb64f01a9d6d4d6ad12
arslankhanali <arslankhanali@gmail.com>
2020-10-10 23:41:41 +1100

    input works

--- poker_arslan.py
+++ poker_arslan.py
@@ -5,6 +5,25 @@ Created on Sat Oct 10 22:18:03 2020
 
 @author: Arslan
 """
+import sys
+
+def 
+
 
 if __name__ == "__main__":
-    print(start)
+    print("Welcome to Poker hand sorter by ARSLAN!!!")
+    print("Provide hands for each player in the format \
+'AH 9S 4D TD 8S 4H JS 3C TC 8D', where the first 5 \
+cards in the line have been dealt to Player 1 and last 5 to player 2")
+    print("Hit enter twice when you have added all hands")
+    all_hands=[]
+    for line in iter(sys.stdin.readline, ''):
+        if line=="\n":
+            break
+        
+        all_hands.append(line.replace('\n',''))
+
+    print("Following hands added")
+    for hand in all_hands:
+        print(hand)
+
